ac686c6f9f0c4dc301051c7990b0fb4cc977f0bd
André Boza <andresantiagoboza@gmail.com>
2023-06-22 20:05:22 -0400

    step 7

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    testIsolation: false,
     baseUrl: 'https://automationexercise.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/1-visiting-site.cy.js
+++ cypress/e2e/1-visiting-site.cy.js
@@ -1,26 +1,42 @@
+const routes = {
+  homepage: '/',
+};
+
 describe('Visiting site and scrolling down ', () => {
   it('visiting site', () => {
     // 1. Visit the exercise site using the base url set in the config
-    cy.visit('/');
+    cy.visit(routes.homepage);
+
     // 2. Scrolls down to the center of the page is a lapse of 3 seconds
     cy.scrollTo('center', { duration: 3000 });
-    // 3. Selecting a product and clincking on it
     cy.get('a[href="/product_details/28"]').click();
-    // 4.
-    cy.get('#quantity').type('{selectAll}{backspace}');
 
+    // 3. Changing amount of products
+    cy.get('#quantity').type('{selectAll}{backspace}');
     cy.get('#quantity').type('30');
 
+    // 4. Adding to cart and viewing cart products
     cy.get('.btn.btn-default.cart').click();
-
     cy.contains('View Cart').click();
 
+    // 5.
     cy.get('.col-sm-6 > .btn').scrollIntoView().should('be.visible');
-
     cy.get('.col-sm-6 > .btn').click();
 
+    // 6.
     cy.get('.modal-body > :nth-child(2) > a > u').click();
 
-    // cy.get('btn.btn-default.check_out').click();
+    // 7. Filling sign up Email and Name fields
+    cy.get('[placeholder="Name"]').type('andre');
+    cy.get('[data-qa="signup-email"]').type('email@ad.com');
+    cy.get('[data-qa="signup-button"]').click();
+
+    //8. 😈
+    // cy.get('form').then((ele) => {
+    //   console.log(ele);
+    //   const radioBtns = ele.find('radio');
+    //   console.log(radioBtns.find(''));
+    // cy.get(radioBtns);
+    // });
   });
 });
